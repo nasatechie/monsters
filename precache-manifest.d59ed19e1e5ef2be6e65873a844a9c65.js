8abada82cfb5d80ff936740f74f5f3ede11496cc
Aptude <aptude@Aptudes-MacBook-Pro.local>
2020-02-22 20:00:10 +0530

    Updates

---
+++ precache-manifest.d59ed19e1e5ef2be6e65873a844a9c65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22b33949363e4d050e2f98d1b0637659",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "586ee1bed426c6fc2f47",
+    "url": "/monsters/static/css/main.af0c4c32.chunk.css"
+  },
+  {
+    "revision": "d5b7dc0e95c86de42d59",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters/static/js/2.0b9572cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "586ee1bed426c6fc2f47",
+    "url": "/monsters/static/js/main.4e6124e5.chunk.js"
+  },
+  {
+    "revision": "69ad86de3903771917ee",
+    "url": "/monsters/static/js/runtime-main.5e6993dc.js"
+  }
+]);
